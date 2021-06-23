8df8f204b315ce0c377f480bb6664df98199d75f
Alexandr Dostovalov <workmaildo@yandex.ru>
2021-06-23 14:06:37 +0500

    feat: add story and statment

---
+++ Triple-X/Triple-X.cpp
@@ -0,0 +1,10 @@
+﻿// Triple-X.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room\n";
+    std::cout << "You need to enter the correct codes to continue...\n";
+}
