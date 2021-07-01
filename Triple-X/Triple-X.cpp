998c99fe677067fa8dbff3dacdad77733f40ee00
Alexandr Dostovalov <workmaildo@yandex.ru>
2021-07-01 13:33:52 +0500

    feat: add levels

--- Triple-X/Triple-X.cpp
+++ Triple-X/Triple-X.cpp
@@ -12,23 +12,17 @@ void print(int number)
 {
     std::cout << number;
 }
-void printWelcome()
+void printWelcome(int level)
 {
-    print("You are a secret agent breaking into a secure server room\n");
+    print("You are a secret agent breaking into a secure server room on level ");
+    print(level);
+    print("\n");
     print("You need to enter the correct codes to continue...\n");
+    print("And you have only two tries\n");
 }
-void playGame()
+
+void printRules(int sum, int product) 
 {
-    printWelcome();
-
-    const int CodeA = 4;
-    const int CodeB = 5;
-    const int CodeC = 5;
-
-    const int sum = CodeA + CodeB + CodeC;
-    const int product = CodeA * CodeB * CodeC;
-
-    // Print statment
     print("\n");
     print("+ There are 3 numbers in the code\n");
     print("+ The codes add-up to: ");
@@ -37,11 +31,24 @@ void playGame()
     print("+ The codes multiply to give: ");
     print(product);
     print("\n");
+}
+bool playGame(int difficulty)
+{
+    printWelcome(difficulty);
+
+    const int CodeA = 4 * difficulty;
+    const int CodeB = 5;
+    const int CodeC = 5;
+
+    const int sum = CodeA + CodeB + CodeC;
+    const int product = CodeA * CodeB * CodeC;
+
+    printRules(sum, product);
 
     //Get users guess
     int GuessA, GuessB, GuessC, GuessSum, GuessProduct;
 
-    int tries = 3;
+    int tries = 2;
     while (true) {
         print("\n");
         std::cin >> GuessA >> GuessB >> GuessC;
@@ -52,13 +59,13 @@ void playGame()
 
         if (GuessSum == sum && GuessProduct == product) {
             print("You win");
-            break;
+            return true;
         }
         print("Your code wrong\n");
         tries--;
         if (tries <= 0) {
             print("You was catched.... \nGAME OVER\n");
-            break;
+            return false;
         }
         print("Tries left - ");
         print(tries);
@@ -69,6 +76,16 @@ void playGame()
 }
 int main()
 {
-    playGame();
+    int levelDifficulty = 1;
+    while (true) {
+        bool isLevelComplete = playGame(levelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+        if (isLevelComplete) {
+            levelDifficulty++;
+        }
+        print("\n******************************\n");
+    }
+    
     return 0;
 }
